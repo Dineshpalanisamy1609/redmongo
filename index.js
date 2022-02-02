13ec58020b30e358c780cf51681cd0f3694d8013
dinesh <dineshmechanical2020@gmail.com>
2022-02-02 09:50:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import DbConnector from './redmongo/lib/activate_db.js';
-import Collection from './redmongo/lib/collections.js';
-import readOps from './redmongo/lib/readOps.js';
-import writeOps from './redmongo/lib/writeOps.js'
-import editOps from './redmongo/lib/updateOps.js';
-import removeOps from './redmongo/lib/removeOps.js';
+import DbConnector from './lib/activate_db.js';
+import Collection from './lib/collections.js';
+import readOps from './lib/readOps.js';
+import writeOps from './lib/writeOps.js'
+import editOps from './lib/updateOps.js';
+import removeOps from './lib/removeOps.js';
 import chalk from 'chalk';
 import { Db } from 'mongodb';
 
