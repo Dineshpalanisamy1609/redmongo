f93eb5fbd62c3147db3f70adf78f18017bd2db7a
dinesh <dineshmechanical2020@gmail.com>
2022-02-02 23:49:03 +0530

    Aggregate Added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import writeOps from './lib/writeOps.js'
 import editOps from './lib/updateOps.js';
 import removeOps from './lib/removeOps.js';
 import chalk from 'chalk';
+import aggregateOps from './lib/aggregate.js';
 import { Db } from 'mongodb';
 
 let log = console.log;
@@ -16,7 +17,15 @@ async function run() {
         useUnifiedTopology: true,
     });
 
-    let testCollection = new Collection().collection('test');
+    const docs = [
+        { stars: 3, categories: ["Bakery", "Sandwiches"], name: "Rising Sun Bakery" },
+        { stars: 4, categories: ["Bakery", "Cafe", "Bar"], name: "Cafe au Late" },
+        { stars: 5, categories: ["Coffee", "Bakery"], name: "Liz's Coffee Bar" },
+        { stars: 3, categories: ["Steak", "Seafood"], name: "Oak Steakhouse" },
+        { stars: 4, categories: ["Bakery", "Dessert"], name: "Petit Cookie" },
+    ];
+
+    let testCollection = new Collection().collection('restaurants');
     // await writeOps.writeOne(testCollection, { a: 1 });
     // await writeOps.writeMany(testCollection, [{ b: 1, c: 1 }, { d: 1, e: 1 }])
     // await readOps.searchOne(testCollection, {});
@@ -25,6 +34,17 @@ async function run() {
     // await editOps.editMany(testCollection, { a: 1 }, { $set: { a: 2 } });
     // await removeOps.removeOne(testCollection, { a: 2 });
     // await removeOps.removeMany(testCollection, { a: 2 });
+
+    // await writeOps.writeMany(testCollection, docs);
+
+    // const pipeline = [
+    //     { $match: { categories: "Bakery" } },
+    //     { $group: { _id: "$stars", count: { $sum: 1 } } }
+    // ];
+
+    // await aggregateOps.aggregate(testCollection, pipeline);
+
+
 }
 
 await run();
